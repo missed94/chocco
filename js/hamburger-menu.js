ecfc1714c44fc62a1a8e41f0f67aea5f3d0c32de
Ivan Filatov <missed94@gmail.com>
2020-06-25 21:56:09 +0700

    полноэкранное меню

---
+++ js/hamburger-menu.js
@@ -0,0 +1,25 @@
+const hamburger = document.querySelector(".hamburger");
+const fullMenu = document.querySelector(".full-menu");
+const closeBtn = document.querySelector(".close-icon");
+const FullMenuItem = document.querySelectorAll(".full-menu-item");
+const wrapper = document.querySelector(".wrapper");
+
+hamburger.addEventListener("click", function (event) {
+  fullMenu.style.left = "0";
+});
+
+closeBtn.addEventListener("click", function (event) {
+  fullMenu.style.left = "100%";
+});
+
+FullMenuItem.forEach(function (element) {
+  element.addEventListener("click", function (event) {
+    fullMenu.style.left = "100%";
+  });
+});
+
+window.addEventListener("resize", function (event) {
+  if (event.currentTarget.innerWidth > 768) {
+    fullMenu.style.left = "100%";
+  }
+});
