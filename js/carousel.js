7d4c94d65da65f6bc4c2d53480b4013ba07c0fad
Ivan Filatov <missed94@gmail.com>
2020-07-02 12:52:00 +0700

    11

--- js/carousel.js
+++ js/carousel.js
@@ -1,8 +1,46 @@
+/* 
+const arrowLeft = $(".slider__arrow-left-wrapper");
+const arrowRight = $(".slider_arrow-right-wrapper");
+const carouselContent = $(".slider-content-list"); */
+
+/* let moove = 0; */
+
+function carousel(arrowRight, arrowLeft, moove, carouselContent) {
+  arrowRight.on("click", function () {
+    moove -= 100;
+
+    if (moove < -100) {
+      moove = 0;
+    }
+
+    return carouselContent.css("left", (moove + "%"));
+  });
+
+  arrowLeft.on("click", function () {
+    moove += 100;
+
+    if (moove > 0) {
+      moove = -100;
+    }
+
+    return carouselContent.css("left", (moove + "%"));
+  });
+
+  
+}
+
+carousel(
+  $(".slider_arrow-right-wrapper"),
+  $(".slider__arrow-left-wrapper"),
+  0,
+  $(".slider-content-list")
+);
+
+/* 
 const arrowLeft = document.querySelector(".slider__arrow-left-wrapper");
 const arrowRight = document.querySelector(".slider_arrow-right-wrapper");
 const carouselContent = document.querySelector(".slider-content-list");
 
-console.log(arrowRight);
 
 let moove = 0;
 
@@ -25,3 +63,4 @@ arrowLeft.addEventListener("click", function (event) {
 
   carouselContent.style.left = moove + "%";
 });
+ */
