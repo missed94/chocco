6864bb5aef6a446ebfff0f938ff9b14162d5993d
Ivan Filatov <missed94@gmail.com>
2020-07-02 14:44:34 +0700

    11

--- js/carousel.js
+++ js/carousel.js
@@ -5,11 +5,15 @@ const carouselContent = $(".slider-content-list"); */
 
 /* let moove = 0; */
 
-function carousel(arrowRight, arrowLeft, moove, carouselContent) {
+ function carousel(arrowRight, arrowLeft, moove, carouselContent) {
+
+  let countChildrenElement = $(carouselContent).children('.slider-content-item').length;
+
+
   arrowRight.on("click", function () {
     moove -= 100;
 
-    if (moove < -100) {
+    if (moove < -((countChildrenElement - 1) * 100)) {
       moove = 0;
     }
 
@@ -20,8 +24,9 @@ function carousel(arrowRight, arrowLeft, moove, carouselContent) {
     moove += 100;
 
     if (moove > 0) {
-      moove = -100;
-    }
+      moove = -((countChildrenElement - 1) * 100);
+      }
+    
 
     return carouselContent.css("left", (moove + "%"));
   });
@@ -34,33 +39,50 @@ carousel(
   $(".slider__arrow-left-wrapper"),
   0,
   $(".slider-content-list")
-);
+); 
 
-/* 
-const arrowLeft = document.querySelector(".slider__arrow-left-wrapper");
+
+/* const arrowLeft = document.querySelector(".slider__arrow-left-wrapper");
 const arrowRight = document.querySelector(".slider_arrow-right-wrapper");
 const carouselContent = document.querySelector(".slider-content-list");
-
+const carouselItem = document.querySelectorAll(".slider-content-item")
 
 let moove = 0;
 
+
+
+function carousel(arrowLeft, arrowRight, carouselContent, moove) {
+
+let countChildrenElement = $(carouselContent).children('.slider-content-item').length;
+
+
 arrowRight.addEventListener("click", function (e) {
+
+  
+
   moove = moove - 100;
 
-  if (moove < -100) {
-    moove = 0;
+  if (moove < -((countChildrenElement - 1) * 100)) {
+  moove = 0;
   }
 
-  carouselContent.style.left = moove + "%";
+
+  return carouselContent.style.left = moove + "%";
 });
 
 arrowLeft.addEventListener("click", function (event) {
+
   moove = moove + 100;
 
   if (moove > 0) {
-    moove = -100;
+  moove = -((countChildrenElement - 1) * 100);
   }
 
-  carouselContent.style.left = moove + "%";
+ 
+  return carouselContent.style.left = moove + "%";
 });
- */
+
+
+}
+
+carousel(arrowLeft, arrowRight, carouselContent, 0, ) */
