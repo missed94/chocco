514746078e5cfa4f94fc8c07bfc9abc5ce71f718
Ivan Filatov <missed94@gmail.com>
2020-07-10 16:38:07 +0700

    22

--- js/video.js
+++ js/video.js
@@ -6,6 +6,7 @@ $(".play-stop").click((e) => {
 
   if (playerContainer.hasClass("player_paused")) {
     playerContainer.removeClass("player_paused");
+    playerContainer.removeClass("player_active");
     player.pause();
   } else {
     playerContainer.addClass("player_paused");
@@ -16,9 +17,7 @@ $(".play-stop").click((e) => {
 $(".playback__slider-line").click((e) => {
   const line = $(e.currentTarget);
   const clickedPos = e.originalEvent.layerX;
-
   const newBtnPosPrecent = (clickedPos / line.width()) * 100;
-  /* const newPlaybackPosSec = (player.duration / 100) * newBtnPosPrecent; */
 
   $(".playback__btn").css({
     left: `${newBtnPosPrecent}%`,
@@ -28,36 +27,47 @@ $(".playback__slider-line").click((e) => {
   player.currentTime = player.duration * (clickedPos / line.width());
   player.play();
   playerContainer.addClass("player_paused");
-  console.log(player.duration);
 });
 
 $(player).on("timeupdate", function () {
   let playBackBtn = $(".playback__btn");
-
   let timeProgress = (player.currentTime / player.duration) * 100 + "%";
-
   playBackBtn.css("left", timeProgress);
 });
 
 $(".volume__slider-line").click((e) => {
   const volumeLine = $(e.currentTarget);
   const volumeClickedPos = e.originalEvent.layerX;
-  playerContainer.removeClass('player__sound-off')
-  const volBtnPosPrecent = (volumeClickedPos / volumeLine.width()) * 100;
-  /* const newPlaybackPosSec = (player.duration / 100) * newBtnPosPrecent; */
 
+  const volBtnPosPrecent = (volumeClickedPos / volumeLine.width()) * 100;
 
   $(".volume__slider-btn").css({
     left: `${volBtnPosPrecent}%`,
   });
-
   player.volume = volBtnPosPrecent / 100;
+
+  if (playerContainer.hasClass("player__sound-off")) {
+    playerContainer.removeClass("player__sound-off");
+    player.muted = false;
+  }
 });
 
+$(".player-elem").on("play", (e) => {
+  playerContainer.addClass("player_active ");
+});
 
+$(".player-splash").on("click", (e) => {
+  playerContainer.addClass("player_active player_paused");
+  player.play();
+});
 
+$(".volume__icons-wrapper").on("click", (e) => {
+  if (playerContainer.hasClass("player__sound-off")) {
+    playerContainer.removeClass("player__sound-off");
+    player.muted = false;
+  } else if ($(e.target).filter("close")) {
+    playerContainer.addClass("player__sound-off");
+    player.muted = true;
+  }
 
-
-
-
-
+});
