d9511059b2338c6e824482fcf8babb94b88b42ce
Ivan Filatov <missed94@gmail.com>
2020-07-15 06:29:48 +0700

    12

--- src/js/vertical-acco.js
+++ src/js/vertical-acco.js
@@ -33,7 +33,16 @@ if (itemContainer.hasClass("acco-item_active")) {
 }) */
 
 
+
+
+
 ;(function() {
+
+
+  const maxWidth = document.body.clientWidth;
+  console.log(maxWidth);
+
+  
   const openAcco = (item) => {
     item.addClass("acco-item_active");
     
@@ -48,30 +57,38 @@ if (itemContainer.hasClass("acco-item_active")) {
   
   $(".acco-item").on("click", (e) => {
     e.preventDefault();
-  
+    let $this = $(e.target);
+    
+
+    if (maxWidth > 480) {
+      if ($(e.target).hasClass("acco-title-wrapper") || $(e.target).hasClass("acco-title")) {
+        
+        const itemContainer = $this.closest(".acco-item");
+        const accoList = itemContainer.closest(".acco-list");
     
     
-  
-    if ($(e.target).hasClass("acco-title-wrapper") || $(e.target).hasClass("acco-title")) {
-      const $this = $(e.target);
-      const itemContainer = $this.closest(".acco-item");
-      const accoList = itemContainer.closest(".acco-list");
-  
-  
-      if (itemContainer.hasClass("acco-item_active")) {
-        closeAcco(accoList);
-      } else {
-        closeAcco(accoList);
-        openAcco(itemContainer);
+        if (itemContainer.hasClass("acco-item_active")) {
+          closeAcco(accoList);
+        } else {
+          closeAcco(accoList);
+          openAcco(itemContainer);
+        }
       }
-    }
+    
+      if ($(e.target).hasClass("close-x") || $(e.target).hasClass("close-vert-menu")) {
+        const target = $(e.target);
+        const Items = target.closest(".acco-list");
+    
+        closeAcco(Items)
+      } else {
+        let $this = $(e.currentTarget);
+console.log($this.html());
+      }
+    } 
+    
+    
   
-    if ($(e.target).hasClass("close-x") || $(e.target).hasClass("close-vert-menu")) {
-      const target = $(e.target);
-      const Items = target.closest(".acco-list");
-  
-      closeAcco(Items)
-    }
+    
   });
   
 })()
